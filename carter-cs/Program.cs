f12e21b1e628464e7aa752849e6d420faf20e8ac
camsdono <cammie2006@outlook.com>
2023-01-16 22:03:05 +0000

    Can now get custom messages and get just the output from the agent

--- carter-cs/Program.cs
+++ carter-cs/Program.cs
@@ -12,9 +12,9 @@ string url = "https://api.carterapi.com/v0/chat";
 var payload = JsonConvert.SerializeObject(new
 {
     api_key = "hnzD16Ejl0A4WWJsnt96Yz8qE9fs0iNw",
-    query = "Hello there",
+    query = "Hello",
     uuid = "admin-1",
-    scene = "bombs_away" // optional!
+    scene = "Bombs_away" // optional!
 });
 
 var client = new HttpClient();
@@ -22,5 +22,9 @@ client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("app
 
 var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
 var response = await client.PostAsync(url, httpContent);
+var json = await response.Content.ReadAsStringAsync();
+Console.WriteLine(json);
+var jsonObject = JsonConvert.DeserializeObject<dynamic>(json);
 
-Console.WriteLine(await response.Content.ReadAsStringAsync());
+string result = jsonObject["output"].text.ToString();
+Console.WriteLine(result);
