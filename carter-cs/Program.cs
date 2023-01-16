a5dbedfbcb63aa866f3b9a41f970ca5201fb66ef
camsdono <cammie2006@outlook.com>
2023-01-16 22:06:31 +0000

    Comit To finsih at school

--- carter-cs/Program.cs
+++ carter-cs/Program.cs
@@ -9,22 +9,29 @@ using System.Net.Http.Headers;
 
 string url = "https://api.carterapi.com/v0/chat";
 
-var payload = JsonConvert.SerializeObject(new
+async void ReachCarter(string apiKey, string carterMessage, string userID, string sceneName) 
 {
-    api_key = "hnzD16Ejl0A4WWJsnt96Yz8qE9fs0iNw",
-    query = "Hello",
-    uuid = "admin-1",
-    scene = "Bombs_away" // optional!
-});
+    var payload = JsonConvert.SerializeObject(new
+    {
+        api_key = apiKey,
+        query = carterMessage,
+        uuid = userID,
+        scene = sceneName
+    });
 
-var client = new HttpClient();
-client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    var client = new HttpClient();
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
-var response = await client.PostAsync(url, httpContent);
-var json = await response.Content.ReadAsStringAsync();
-Console.WriteLine(json);
-var jsonObject = JsonConvert.DeserializeObject<dynamic>(json);
+    var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
+    var response = await client.PostAsync(url, httpContent);
+    var json = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(json);
+    var jsonObject = JsonConvert.DeserializeObject<dynamic>(json);
 
-string result = jsonObject["output"].text.ToString();
-Console.WriteLine(result);
+    string result = jsonObject["output"].text.ToString();
+    Console.WriteLine(result);
+}
+
+ReachCarter("hnzD16Ejl0A4WWJsnt96Yz8qE9fs0iNw", "Hello", "admin-1", "bombs_away");
+
+
